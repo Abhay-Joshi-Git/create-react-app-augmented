c1a7a98843fe4ff820b79af679814804c0b90c8b
Abhay Joshi <abhy.joshi@gmail.com>
2018-02-19 11:34:39 +0530

    added liniting - added eslint config for airbnb

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div style={{ padding: '15px 0px' }}>
+      <img src="https://reactjs.org/logo-og.png" alt="test" />
+    </div>
+  </div>
+);
+
+export default App;
