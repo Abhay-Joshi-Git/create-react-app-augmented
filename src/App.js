17ba1b666be48e086dfa304b7645279a099bbf01
Abhay Joshi <abhy.joshi@gmail.com>
2018-02-19 14:26:57 +0530

    added - 1. role and click event handler for div 2. added editorconfig, remember to add configuration for settings.json file for format on save by prettier 3. tried adding config for debugging in VSCode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
 
+/* eslint-disable no-console, no-param-reassign */
+
 const App = () => (
   <div className="App">
-    <div style={{ padding: '15px 0px' }}>
+    <div
+      role="presentation"
+      style={{ padding: '15px 0px' }}
+      onClick={() => {
+        const c = 30;
+        console.log(c);
+      }}
+    >
       <img src="https://reactjs.org/logo-og.png" alt="test" />
     </div>
   </div>
