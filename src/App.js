ee47c40bd51d6bb57bfcd02359cbab1f31b92a72
Abhay Joshi <abhy.joshi@gmail.com>
2018-02-20 12:59:49 +0530

    added clearFix example

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
 import React from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Clearfix } from 'react-bootstrap';
 import './App.css';
 
 /* eslint-disable no-console, no-param-reassign */
 
 const App = () => (
   <div className="App">
+    <div>
+      Some Header
+    </div>
     <Grid className="container">
       <Row>
-        <Col md={5} xs={12} className="logo-container">
+        <Col md={5} sm={12} className="logo-container">
           <i className="fab fa-fort-awesome fa-lg" style={{ color: 'white' }} />
         </Col>
-        <Col md={5} mdOffset={2} xs={12} className="logo-container">
+        <Col md={5} mdOffset={2} sm={12} className="logo-container">
+          <i className="fas fa-adjust" style={{ color: 'white' }} />
+        </Col>
+        <Col md={5} sm={12} className="logo-container" style={{ height: 60 }}>
+          <div>Some content</div>
+        </Col>
+        <Col md={5} mdOffset={2} sm={12} className="logo-container">
           <i className="fas fa-adjust" style={{ color: 'white' }} />
         </Col>
       </Row>
-      <Row>
+      <Row md={12}>
         <div
           role="presentation"
-          style={{ padding: '15px 0px' }}
+          style={{ padding: '15px 0px', width: '100%' }}
           onClick={() => {
             const c = 30;
             console.log(c);
           }}
         >
-          <img src="https://reactjs.org/logo-og.png" alt="test" />
+          <img src="https://reactjs.org/logo-og.png" alt="test" style={{ width: '100%' }} />
         </div>
       </Row>
+
     </Grid>
+    <br />
+    <Clearfix style={{ backgroundColor: 'lightBlue' }}>
+      <div className="btn btn-secondary fa-pull-left">
+        floated left
+      </div>
+      <div className="btn btn-secondary fa-pull-right">
+        floated right
+      </div>
+    </Clearfix>
+    <span>test</span>
   </div>
 );
 
